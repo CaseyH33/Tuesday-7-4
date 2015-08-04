131bb84e720f27e90602e7de7f43d5a99e315fbc
Casey Heitz & Mike Chastain <student@epicodus.com>
2015-08-04 12:07:38 -0700

    Created a fully functional fully operation battlestation..er..Cd App in Silex

--- CD/app/app.php
+++ CD/app/app.php
@@ -5,9 +5,35 @@
 
   $app = new Silex\Application();
 
-  $app->("/",function()
+  $app->get("/",function()
   {
-    return"Home";
+    $first_cd = new CD("Master of Reality","Black Sabbath","images/sabbath.jpg",10.99);
+    $second_cd = new CD("Electric Larryland","Butthole Surfers", "images/larryland.jpg",10.99);
+    $third_cd = new CD("Neveryoumind","Nirvana","images/nirvana.jpg",10.99);
+    $fourth_cd = new CD("Pork Lion", "Snoop Pork","images/pork.jpg",49.99);
+
+$cds = array($first_cd,$second_cd,$third_cd,$fourth_cd);
+
+    $output = "";
+    foreach ($cds as $album){
+
+      $output .= "<div class='row'>
+      <div class='col-md-6'>
+      <img src=" . $album->getCoverArt() . " width='25%' >
+      </div>
+      <div class ='col-md-6'>
+      <p>" . $album->getTitle() . "</p>
+      <p>By " . $album->getArtist() . "</p>
+      <p>$" . $album->getPrice() . "</p>
+    </div>
+    </div>
+    ";
+
+
+    }
+
+  return $output;
+
   });
 return $app;
 
