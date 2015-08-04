7eb08179b738438abc65fa1de7ce04e5b213a1f2
Casey Heitz & Mike Chastain <student@epicodus.com>
2015-08-04 12:20:28 -0700

    Initialize car app

---
+++ Car/app/app.php
@@ -0,0 +1,13 @@
+<?php
+  require_once __DIR__."/../vendor/autoload.php";
+  require_once __DIR__."/../src/Car.php";
+
+  $app = new Silex\Application();
+
+  $app->get("/", function(){
+    return "Hello";
+  });
+
+return $app;
+
+?>
